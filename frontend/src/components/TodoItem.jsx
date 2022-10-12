05bf28be1de2513d6cb1d53d7de364d8b0f61281
Gavin McDonald <gavinmcdonald@protonmail.com>
2022-10-12 17:22:45 +0100

    Format code

--- frontend/src/components/TodoItem.jsx
+++ frontend/src/components/TodoItem.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { useState } from 'react'
+import {useState} from 'react'
 
-const TodoItem = ({ id, title, complete }) => {
+const TodoItem = ({id, title, complete}) => {
 
-    const [ isComplete, setIsComplete ] = useState(complete)
+    const [isComplete, setIsComplete] = useState(complete)
 
     const handleChange = () => {
         setIsComplete(!isComplete)
     }
 
     return (
-        <li className="todo-list__item" data-complete={ isComplete }>
-            <input type="checkbox" onChange={ handleChange } checked={ isComplete } />
-            { title }
+        <li className="todo-list__item" data-complete={isComplete}>
+            <input type="checkbox" onChange={handleChange} checked={isComplete}/>
+            {title}
         </li>
     )
 }
 
 export default TodoItem
+
+
